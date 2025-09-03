224d3acd75e225903b933c396641e63656c7539d
Gopalakrishnan a <126150922+bsgopal@users.noreply.github.com>
2025-09-03 22:02:47 +0530

    Update db.js

--- jewel-scheme-backend/config/db.js
+++ jewel-scheme-backend/config/db.js
@@ -1,20 +1,15 @@
-// db.js - updated for ES6
+// db.js - hybrid config for local + Render
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// console.log("DB_HOST:", process.env.DB_HOST);
-// console.log("DB_USER:", process.env.DB_USER);
-// console.log("DB_NAME:", process.env.DB_NAME);
-// console.log("DB_PORT:", process.env.DB_PORT);
-
 const pool = mysql.createPool({
-  host: "localhost",       // your DB host
-  user: "root",            // your DB username
-  password: "Gopalyuvi@5293",      // your DB password
-  database: "jewel_scheme", // your DB name
-  port: 3306,              // your DB port
+  host: process.env.DB_HOST || "localhost",       // fallback to localhost
+  user: process.env.DB_USER || "root",            // fallback to root
+  password: process.env.DB_PASSWORD || "Gopalyuvi@5293", // fallback to local password
+  database: process.env.DB_NAME || "jewel_scheme", // fallback DB
+  port: process.env.DB_PORT || 3306,              // fallback port
   connectionLimit: 10,
 });
 
